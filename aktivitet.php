4e263891d265d53bcf8eefd854217fdd1bd9a8b8
Margrethe Thorsen <margrethethorsen@gmail.com>
2017-04-05 10:15:22 +0200

    Lagt til googlemaps over Blaa
    Det er også kommentert ut addressen til de andre campusene.

--- aktivitet.php
+++ aktivitet.php
@@ -8,3 +8,21 @@ test
 <?php
     require 'footer.php'
 ?>
+
+<div id="googleMap" style="width:400px;height:400px;">
+    <script>
+        function myMap() {
+        var mapProp= {
+        center:new google.maps.LatLng(59.920543, 10.752696),
+        zoom:17,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+    </script>
+    
+    <!-- Lat&Lng brenneriveien '59.920543, 10.752696' -->
+    <!-- Lat&Lng vulkan '59.922562, 10.751683' -->
+    <!-- I TILFELLE: Lat&Lng fjerdingen '59.916238, 10.759765' -->
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9tygoWPfsio74yQq3wXl6wxU23ERPlgA&callback=myMap"></script>
+</div>
