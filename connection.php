3509193c92b4d84a1c35f092e90290ce6a64939d
Alexander <garale16@student.westerdals.no>
2017-04-05 16:07:48 +0200

    Add files via upload
    La til flere eksempelartikkler i test.sql
    Fjernet eksempelartikkler i index.php
    La til SQL kode i connectionr.php for å hente artikkel lagt til sist først og limitere antall artikkler til 10
    Fjernet "pictures/" fra test-article1.php

--- connection.php
+++ connection.php
@@ -7,8 +7,9 @@ $name = 'test';
 
     $connection = new PDO("mysql:host=localhost;dbname={$name};port={$port}", $username, $password);
 
-    $statement = $connection->prepare('SELECT * FROM articles');
+    $statement = $connection->prepare('SELECT * FROM articles ORDER BY id DESC LIMIT 10');
     $statement->execute();
+    //Order by desc for å hente sist innlagte artikler først, kan evt. legge til og order by dato (kan også bruke carbom for ekstrapoeng)
 
     $arts = [];
 
--- index.php
+++ index.php
@@ -12,6 +12,7 @@
     <div class="artikel1">
         <article>
             
+            <!--- Printer alle artikler --->
             <?php foreach ($articles as $event) 
                 {  
                     require 'article.php';
@@ -29,66 +30,16 @@
             <hr>
             <br>
             <!---Artikkel 2 start --->
-            <?php 
-                require 'test-article2.php'
-            ?>
+            <a href="http://www.harvard.edu" target="_blank">HARVARD UNIVERSITY / THE BEST SCHOOL / FEBRUARY 21, 2017</a> 
+            <h2>ANDRE ARTIKEL</h2>
+            BY <a class="av" href="http://www.facebook.com/saccky?fref=ts" target="_blank">KENNETH SACCKY</a>
+            <p>
+                <img class="me" width="200" src="Einstein.jpg">
+                Cable bills in America have been on the rise, with the average household spending $64.41 a month or about $768 a year. The thing is, there are many cheaper alternatives available these days that will provide you with enough entertainment options for the entire family. You can try a third-party service such as Netflix, which costs $9.99 a month, Amazon Prime at $99 a year or Hulu Plus, which costs $7.99 a month.
+
+                If you prefer to stick to television, but usually only watch the same few channels, you can still lower costs by buying a package of channels. Sling TV does a base package called “The Best of Live TV,” which is $20 a month and includes 19 of the most popular channels. Add-on packs are an extra $5 each per month, so you can tailor it to your family’s needs and still save money.
+            </p>
             <!---Artikkel 2 slutt --->
-            
-            <hr>
-            <br>
-            <!---Artikkel 3 start --->
-            <a href="http://www.harvard.edu" target="_blank">HARVARD UNIVERSITY / THE BEST SCHOOL / FEBRUARY 21, 2017</a> 
-            <h2>TREDJE ARTIKEL</h2>
-            BY <a class="av" href="http://www.facebook.com/saccky?fref=ts" target="_blank">KENNETH SACCKY</a>
-            <p>
-                <img class="me" width="200" src="Einstein.jpg">
-                Cable bills in America have been on the rise, with the average household spending $64.41 a month or about $768 a year. The thing is, there are many cheaper alternatives available these days that will provide you with enough entertainment options for the entire family. You can try a third-party service such as Netflix, which costs $9.99 a month, Amazon Prime at $99 a year or Hulu Plus, which costs $7.99 a month.
-
-                If you prefer to stick to television, but usually only watch the same few channels, you can still lower costs by buying a package of channels. Sling TV does a base package called “The Best of Live TV,” which is $20 a month and includes 19 of the most popular channels. Add-on packs are an extra $5 each per month, so you can tailor it to your family’s needs and still save money.
-            </p>
-            <!---Artikkel 3 slutt --->
-            
-            <hr>
-            <br>
-            <!---Artikkel 4 start --->
-            <a href="http://www.harvard.edu" target="_blank">HARVARD UNIVERSITY / THE BEST SCHOOL / FEBRUARY 21, 2017</a> 
-            <h2>FJÄRDE ARTIKEL</h2>
-            BY <a class="av" href="http://www.facebook.com/saccky?fref=ts" target="_blank">KENNETH SACCKY</a>
-            <p>
-                <img class="me" width="200" src="Einstein.jpg">
-                Cable bills in America have been on the rise, with the average household spending $64.41 a month or about $768 a year. The thing is, there are many cheaper alternatives available these days that will provide you with enough entertainment options for the entire family. You can try a third-party service such as Netflix, which costs $9.99 a month, Amazon Prime at $99 a year or Hulu Plus, which costs $7.99 a month.
-
-                If you prefer to stick to television, but usually only watch the same few channels, you can still lower costs by buying a package of channels. Sling TV does a base package called “The Best of Live TV,” which is $20 a month and includes 19 of the most popular channels. Add-on packs are an extra $5 each per month, so you can tailor it to your family’s needs and still save money.
-            </p>
-            <!---Artikkel 4 slutt --->
-            
-            <hr>
-            <br>
-            <!---Artikkel 5 start --->
-            <a href="http://www.harvard.edu" target="_blank">HARVARD UNIVERSITY / THE BEST SCHOOL / FEBRUARY 21, 2017</a> 
-            <h2>FEMTE ARTIKEL</h2>
-            BY <a class="av" href="http://www.facebook.com/saccky?fref=ts" target="_blank">KENNETH SACCKY</a>
-            <p>
-                <img class="me" width="200" src="Einstein.jpg">
-                Cable bills in America have been on the rise, with the average household spending $64.41 a month or about $768 a year. The thing is, there are many cheaper alternatives available these days that will provide you with enough entertainment options for the entire family. You can try a third-party service such as Netflix, which costs $9.99 a month, Amazon Prime at $99 a year or Hulu Plus, which costs $7.99 a month.
-
-                If you prefer to stick to television, but usually only watch the same few channels, you can still lower costs by buying a package of channels. Sling TV does a base package called “The Best of Live TV,” which is $20 a month and includes 19 of the most popular channels. Add-on packs are an extra $5 each per month, so you can tailor it to your family’s needs and still save money.
-            </p>
-            <!---Artikkel 5 slutt --->
-            
-            <hr>
-            <br>
-            <!---Artikkel 6 start --->
-            <a href="http://www.harvard.edu" target="_blank">HARVARD UNIVERSITY / THE BEST SCHOOL / FEBRUARY 21, 2017</a> 
-            <h2>SJETTE ARTIKEL</h2>
-            BY <a class="av" href="http://www.facebook.com/saccky?fref=ts" target="_blank">KENNETH SACCKY</a>
-            <p>
-                <img class="me" width="200" src="Einstein.jpg">
-                Cable bills in America have been on the rise, with the average household spending $64.41 a month or about $768 a year. The thing is, there are many cheaper alternatives available these days that will provide you with enough entertainment options for the entire family. You can try a third-party service such as Netflix, which costs $9.99 a month, Amazon Prime at $99 a year or Hulu Plus, which costs $7.99 a month.
-
-                If you prefer to stick to television, but usually only watch the same few channels, you can still lower costs by buying a package of channels. Sling TV does a base package called “The Best of Live TV,” which is $20 a month and includes 19 of the most popular channels. Add-on packs are an extra $5 each per month, so you can tailor it to your family’s needs and still save money.
-            </p>
-            <!---Artikkel 6 slutt --->
         
         </article>       
     </div>
