edfa1f3342cbff15c6b41e6dddcc3e3a48d2b96e
Margrethe Thorsen <margrethethorsen@gmail.com>
2017-04-06 12:54:41 +0200

    Brukte beautifyer.io til å rydde opp i koden på enkelte filer

--- header.php
+++ header.php
@@ -1,24 +1,22 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="stylesheet" href="main.css"/>
-        <link rel="icon" href="logo1.png" />
-        <title>WestGuide</title>
-    </head>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="main.css" />
+    <link rel="icon" href="logo1.png" />
+    <title>WestGuide</title>
+</head>
+
 <body>
-    
     <div id="wrapper">
         <header>
-            <a href="index.php">
-                <img id="logo" src="logo-klipt.png" width="300px">
-            </a>
+            <a href="index.php"> <img id="logo" src="logo-klipt.png" width="300px"> </a>
         </header>
-
         <nav>
             <ul>
                 <li><a href="index.php">Forsiden</a></li>
@@ -32,6 +30,6 @@
                 </li>
                 <li><a href="aktivitet.php">AKTIVITET</a></li>
                 <li><a href="mat.php">MAT</a></li>
-                <li><a href="skole.php">SKOLE</a></li> 
+                <li><a href="skole.php">SKOLE</a></li>
             </ul>
         </nav>
--- index.php
+++ index.php
@@ -2,16 +2,12 @@
     require 'connection.php';
     require 'header.php';
 ?>
-    
+    <br> <img src="westerdals_bygning_01-1-ekempel.jpg" width="100%">
     <br>
-    <img src="westerdals_bygning_01-1-ekempel.jpg" width="100%">
-    <br>
-
     <hr>
     <br>
     <div class="artikel1">
         <article>
-            
             <!--- Printer alle artikler --->
             <?php foreach ($articles as $event) 
                 {  
@@ -19,31 +15,20 @@
                 ?>
                 <hr>
                 <br>
-            <?php  } ?>
-            
-            <!---Artikkel 1 start --->
-            <?php 
+                <?php  } ?>
+                    <!---Artikkel 1 start --->
+                    <?php 
                 require 'test-article1.php'
             ?>
-            <!---Artikkel 1 slutt --->
-            
-            <hr>
-            <br>
-            <!---Artikkel 2 start --->
-            <a href="http://www.harvard.edu" target="_blank">HARVARD UNIVERSITY / THE BEST SCHOOL / FEBRUARY 21, 2017</a> 
-            <h2>ANDRE ARTIKEL</h2>
-            BY <a class="av" href="http://www.facebook.com/saccky?fref=ts" target="_blank">KENNETH SACCKY</a>
-            <p>
-                <img class="me" width="200" src="Einstein.jpg">
-                Cable bills in America have been on the rise, with the average household spending $64.41 a month or about $768 a year. The thing is, there are many cheaper alternatives available these days that will provide you with enough entertainment options for the entire family. You can try a third-party service such as Netflix, which costs $9.99 a month, Amazon Prime at $99 a year or Hulu Plus, which costs $7.99 a month.
-
-                If you prefer to stick to television, but usually only watch the same few channels, you can still lower costs by buying a package of channels. Sling TV does a base package called “The Best of Live TV,” which is $20 a month and includes 19 of the most popular channels. Add-on packs are an extra $5 each per month, so you can tailor it to your family’s needs and still save money.
-            </p>
-            <!---Artikkel 2 slutt --->
-        
-        </article>       
+                        <!---Artikkel 1 slutt --->
+                        <hr>
+                        <br>
+                        <!---Artikkel 2 start ---><a href="http://www.harvard.edu" target="_blank">HARVARD UNIVERSITY / THE BEST SCHOOL / FEBRUARY 21, 2017</a>
+                        <h2>ANDRE ARTIKEL</h2> BY <a class="av" href="http://www.facebook.com/saccky?fref=ts" target="_blank">KENNETH SACCKY</a>
+                        <p> <img class="me" width="200" src="Einstein.jpg"> Cable bills in America have been on the rise, with the average household spending $64.41 a month or about $768 a year. The thing is, there are many cheaper alternatives available these days that will provide you with enough entertainment options for the entire family. You can try a third-party service such as Netflix, which costs $9.99 a month, Amazon Prime at $99 a year or Hulu Plus, which costs $7.99 a month. If you prefer to stick to television, but usually only watch the same few channels, you can still lower costs by buying a package of channels. Sling TV does a base package called “The Best of Live TV,” which is $20 a month and includes 19 of the most popular channels. Add-on packs are an extra $5 each per month, so you can tailor it to your family’s needs and still save money. </p>
+                        <!---Artikkel 2 slutt --->
+        </article>
     </div>
-    
     <?php 
         require 'footer.php' 
-    ?> 
+    ?>
