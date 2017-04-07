9df41b583a739e228ce0d5d9aae24f97e6ee2c00
Alexander <garale16@student.westerdals.no>
2017-04-07 21:30:48 +0200

    Add files via upload
    La til forslag til conncection-event-footer.php i kode på mat.php og kultur.php
    La til sortering etter tags i mat.php og kultur.php

--- kultur.php
+++ kultur.php
@@ -1,6 +1,23 @@
 <?php
     require 'connection-event.php';
     require 'header.php';
+
+    /* Kode for spesefik spørring etter events med tag "Kultur" (Ikke CAPS-sensetiv) */
+    $statement = $connection->prepare('SELECT * FROM events WHERE tag = "KULTUR" ORDER BY id DESC LIMIT 10 ');
+
+    /* 
+    <!--- connection-event-footer.php foreslått start ---> 
+    <?php */
+    $statement->execute();
+
+    $events = [];
+
+    while($row = $statement->fetch(PDO::FETCH_ASSOC)) 
+    {
+	   $events[] = $row;
+    } 
+    /* connection-event-footer.php slutt */
+
 ?>
 
     <h1>KULTUR</h1>
--- mat.php
+++ mat.php
@@ -1,9 +1,35 @@
 <?php
+    require 'connection-event.php';
     require 'header.php';
+
+    /* Kode for spesefik spørring etter events med tag "kultur" (Ikke CAPS-sensetiv) */
+    $statement = $connection->prepare('SELECT * FROM events WHERE tag = "mat" ORDER BY id DESC LIMIT 10 ');
+
+    /* 
+    <!--- connection-event-footer.php foreslått start ---> 
+    <?php */
+    $statement->execute();
+
+    $events = [];
+
+    while($row = $statement->fetch(PDO::FETCH_ASSOC)) 
+    {
+	   $events[] = $row;
+    } 
+    /* connection-event-footer.php slutt */
+
 ?>
 
-<h1>MAT</h1>
-
+    <h1>MAT</h1>
+    <!--- Printer alle events med tag = kultur --->
+    <?php 
+        foreach ($events as $event) 
+        {  
+            require 'event.php';
+        ?>
+            <hr>
+            <br>
+        <?php  } ?>
 <?php
     require 'footer.php';
 ?>
