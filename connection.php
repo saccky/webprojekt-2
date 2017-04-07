8c8704d7373956bd3e9ab32b33774c960ae54e25
Alexander <garale16@student.westerdals.no>
2017-04-07 22:19:04 +0200

    Delete connection.php
    gammel versjon, ny er connection-header

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$port = 8889;
-$username = 'root';
-$password = 'root';
-$name = 'westguide';
-
-    $connection = new PDO("mysql:host=localhost;dbname={$name};port={$port}", $username, $password);
