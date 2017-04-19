508b682906c07bf508ed18d0143669aedb0ab766
LindaHBN <linda.b.n@hotmail.com>
2017-04-19 13:41:05 +0200

    har lagt til logoer til sosiale medier

--- header.php
+++ header.php
@@ -3,43 +3,56 @@
     header('Content-Type: text/html; charset=ISO-8859-1');
 ?>
 
-<!DOCTYPE html>
-<html>
+    <!DOCTYPE html>
+    <html>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="stylesheet" href="main.css" />
-    <link rel="icon" href="logo1.png" />
-    <title>WestGuide</title>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="stylesheet" href="main.css" />
+        <link rel="icon" href="logo1.png" />
+        <title>WestGuide</title>
+    </head>
 
-<body>
-    <div id="wrapper">
-        <header>
+    <body>
+        <div id="wrapper">
+            <header>
 
-            <a href="index.php">
-                <img id="logo" src="logo-klipt.png" width="400px">
-            </a>
-        </header>
-        <nav>
-            <ul>
-                <li><a href="index.php">Forsiden</a></li>
-                <li><a href="kultur.php">KULTUR</a>
-                    <ul>
-                        <li><a href="#">Kunst</a></li>
-                        <li><a href="#">Utstilling</a></li>
-                        <li><a href="#">Utstilling</a></li>
-                        <li><a href="#">Utstilling</a></li>
-                    </ul>
-                </li>
-                <li><a href="aktivitet.php">AKTIVITET</a></li>
-                <li><a href="mat.php">MAT</a></li>
-                <li><a href="skole.php">SKOLE</a></li>
-                <li><a href="testside.php">TEST-SIDE</a></li>
-                <li><a href="article-event.php">articleevent</a></li>
-            </ul>
-        </nav>
+                <a href="index.php">
+                    <img id="logo" src="logo-klipt.png" width="400px">
+                </a>
+
+
+
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Forsiden</a></li>
+                    <li><a href="kultur.php">KULTUR</a>
+
+                        <img class="logoer" id="facelogo" src="facebook-logo.png">
+
+
+                        <img class="logoer" id="twitterlogo" src="twitter-logo.png">
+
+
+                        <img class="logoer" id="instalogo" src="instagrom-logo.png">
+
+
+                        <ul>
+                            <li><a href="#">Kunst</a></li>
+                            <li><a href="#">Utstilling</a></li>
+                            <li><a href="#">Utstilling</a></li>
+                            <li><a href="#">Utstilling</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="aktivitet.php">AKTIVITET</a></li>
+                    <li><a href="mat.php">MAT</a></li>
+                    <li><a href="skole.php">SKOLE</a></li>
+                    <li><a href="testside.php">TEST-SIDE</a></li>
+                    <li><a href="article-event.php">articleevent</a></li>
+                </ul>
+            </nav>
