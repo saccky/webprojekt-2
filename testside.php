c86551268012bff41e1ea92d2782191323e0ae15
Alexander <garale16@student.westerdals.no>
2017-04-19 12:27:39 +0200

    Add files via upload
    La inn variabler i stede for variabler i Google Maps koden, litt ryddigere, kanskje mulighet for å linke til koordinater i event "where" område i fremtiden.

--- testside.php
+++ testside.php
@@ -16,19 +16,24 @@
 
     <div id="googleMap" style="width:400px;height:400px;">
         <script>
+            //Lat&Lng Brenneriveien '59.920543, 10.752696'
+            //Lat&Lng Vulkan '59.922562, 10.751683'
+            //Lat&Lng Fjerdingen '59.916238, 10.759765'
+            var LatBrenneriveien = 59.920543;
+            var LngBrenneriveien = 10.752696;
+            var LatVulkan = 59.922562;
+            var LngVulkan = 10.751683;
+            var LatFjerdingen = 59.916238;
+            var LngFjerdingen = 10.759765;
+            
             function myMap() 
             {
-                var mapProp = {
-                    center: new google.maps.LatLng(59.920543, 10.752696)
-                    , zoom: 17
-                , };
+                var mapProp = {center: new google.maps.LatLng(LatBrenneriveien, LngBrenneriveien), zoom: 17};
                 var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
             }
         </script>
-        <!-- Lat&Lng brenneriveien '59.920543, 10.752696' -->
-        <!-- Lat&Lng vulkan '59.922562, 10.751683' -->
-        <!-- I TILFELLE: Lat&Lng fjerdingen '59.916238, 10.759765' -->
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9tygoWPfsio74yQq3wXl6wxU23ERPlgA&callback=myMap"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9tygoWPfsio74yQq3wXl6wxU23ERPlgA&callback=myMap">
+        </script>
     </div>
     
     <br>
