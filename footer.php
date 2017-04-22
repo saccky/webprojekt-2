e7f909837ad38b2bd065eb9705e0308e24061e6e
saccky <kennethsaccky@gmail.com>
2017-04-22 09:08:34 +0200

    test
    test

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
-            <div class="mainFooter">
-                  <img class="logoer" id="facelogo" src="./logoer/facebook-logo.png">
-            <img class="logoer" id="twitterlogo" src="./logoer/twitter-logo.png">
-            <img class="logoer" id="instalogo" src="./logoer/instagrom-logo.png">
+
 
                 <footer>
+                     <img class="logoer" id="facelogo" src="./logoer/facebook-logo.png">
+            <img class="logoer" id="twitterlogo" src="./logoer/twitter-logo.png">
+            <img class="logoer" id="instalogo" src="./logoer/instagrom-logo.png">
                      &copy;2017 Laget av Grupp 32 
                 </footer>
-            </div>
+        
 
         </div>
     </body>
