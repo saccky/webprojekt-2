44585be99d1a88537a3429a930d418d27554c3f5
Alexander <garale16@student.westerdals.no>
2017-04-23 12:17:15 +0200

    Små oppdateringer før midlertidig levering
    Ryddet i main.css, footer.php, og header.php
    Matchet lengde på footer til nav bar
    Små forbedringer

--- footer.php
+++ footer.php
@@ -1,13 +1,12 @@
 
 
                 <footer>
-                     <img class="logoer" id="facelogo" src="./logoer/facebook-logo.png">
-            <img class="logoer" id="twitterlogo" src="./logoer/twitter-logo.png">
-            <img class="logoer" id="instalogo" src="./logoer/instagrom-logo.png">
-                     &copy;2017 Laget av Grupp 32 
+                    <img class="logoer" id="facelogo" src="./logoer/facebook-logo.png">
+                    <img class="logoer" id="twitterlogo" src="./logoer/twitter-logo.png">
+                    <img class="logoer" id="instalogo" src="./logoer/instagrom-logo.png">
+                    &copy;2017 Laget av Grupp 32
                 </footer>
-        
 
-        </div>
+        <!---/div> <!--- #wrapper lukker --->
     </body>
 </html>
--- header.php
+++ header.php
@@ -12,27 +12,27 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
-          <link rel="stylesheet" href="normalize.css" />
+        <!--- link rel="stylesheet" href="normalize.css" / --->
         <link rel="stylesheet" href="main.css" />
         <link rel="icon" href="logo1.png" />
         <title>WestGuide</title>
     </head>
 
-   <!--<body>
-        <div id="wrapper"> -->
+    <body>
+        <!--- div id="wrapper" --->
             <header>
 
                 <a href="index.php">
                     <img id="logo" src="./logoer/logo-klipt.png" width="400px">
                 </a>
 
-</header>
+            </header>
           
             <nav>
                 <ul>
-                   <li><a href="index.php">HJEM</li> 
-                       <li><a href="aktivitet.php">AKTIVITET</li>
-                        <li><a href="kultur.php">KULTUR</li>
-                            <li><a href="skole.php">SKOLE</li>
+                    <li><a href="index.php">HJEM</a></li> 
+                    <li><a href="aktivitet.php">AKTIVITET</a></li>
+                    <li><a href="kultur.php">KULTUR</a></li>
+                    <li><a href="skole.php">SKOLE</a></li>
                 </ul>
             </nav>
