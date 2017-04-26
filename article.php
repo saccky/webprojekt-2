f29d283f60835045bb44abebd3e4dfd4ac667ac4
grdall <garale16@student.westerdals.no>
2017-04-26 11:04:07 +0200

    "Se hendelse" knapp
    Vises nå bare hvis artikkelen har en fremmednøkkel til events-table, for
    alle artikler

--- article.php
+++ article.php
@@ -17,16 +17,14 @@ BY
     <button class="article-delete-btn" type="button"> Slett Artikkel </button>
 </a>
 
-<script>
-    document.getElementById("article-event-link").style.display = "block";
-    //Gjemmer knappen hvis det ikke er noe å linke til //Virker kun på den siste artikkelen printet
-    if(<?= $event['article-event-fk'] ?>) //hvis fremmednøkkelen true (ikke er null), vis knapp
-    {
-        document.getElementById("article-event-link").style.display = "block";
-    }
-</script>
 
-<a id="article-event-link" href="article-event.php?id=<?= $event['article-event-fk'] ?>">
-    <button class="article-event-link-btn" type="button"> Se Hendelsen </button>
-</a>
+<?php
+    //hvis fremmednøkkelen er true (ikke er null), legg til knapp
+    if($event['article-event-fk']) 
+    { ?>
+        <a id="article-event-link" href="article-event.php?id=<?= $event['article-event-fk'] ?>">
+            <button class="article-event-link-btn" type="button"> Se Hendelsen </button>
+        </a>
+    <?php } ?>
+
 <!---Artikkel slutt --->
