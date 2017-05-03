6bc13c6ce2a131ab7fdbac7d6c2b165332368c2b
Margrethe Thorsen <margrethethorsen@gmail.com>
2017-05-03 11:29:44 +0200

    Lagt til php og wrapper
    Lagt card.php til filen og lagt en wrapper til article-filen

--- article.php
+++ article.php
@@ -1,32 +1,34 @@
 <!---Artikkel start --->
-<a href="<?= $event['article-link'] ?>" target="_blank"><?= $event['article-link-name'] ?></a>
+<div if="article-card">
+    <a href="<?= $event['article-link'] ?>" target="_blank"><?= $event['article-link-name'] ?></a>
 
-<h2><?= $event['article-title'] ?></h2>
-BY 
-<a class="av" href="<?= $event['article-author-link'] ?>" target="_blank">
-    <?= $event['article-author'] ?>
-</a>
+    <h2><?= $event['article-title'] ?></h2>
+    BY 
+    <a class="av" href="<?= $event['article-author-link'] ?>" target="_blank">
+        <?= $event['article-author'] ?>
+    </a>
 
-<p>
-    <img class="me" width="200" src="<?= $event['article-image-path'] ?>">
-    <?= $event['article-body'] ?>
-</p>
+    <p>
+        <img class="me" width="200" src="<?= $event['article-image-path'] ?>">
+        <?= $event['article-body'] ?>
+    </p>
 
-<!--- Sletteknapp --->
-<a class="article-delete-class" href="delete.php?id=<?= $event['id'] ?>">
-    <button class="article-delete-btn" type="button"> Slett Artikkel </button>
-</a>
+    <!--- Sletteknapp --->
+    <a class="article-delete-class" href="delete.php?id=<?= $event['id'] ?>">
+        <button class="article-delete-btn" type="button"> Slett Artikkel </button>
+    </a>
 
-<script>
-    document.getElementById("article-event-link").style.display = "block";
-    //Gjemmer knappen hvis det ikke er noe å linke til //Virker kun på den siste artikkelen printet
-    if(<?= $event['article-event-fk'] ?>) //hvis fremmednøkkelen true (ikke er null), vis knapp
-    {
+    <script>
         document.getElementById("article-event-link").style.display = "block";
-    }
-</script>
+        //Gjemmer knappen hvis det ikke er noe å linke til //Virker kun på den siste artikkelen printet
+        if(<?= $event['article-event-fk'] ?>) //hvis fremmednøkkelen true (ikke er null), vis knapp
+        {
+            document.getElementById("article-event-link").style.display = "block";
+        }
+    </script>
 
-<a id="article-event-link" href="article-event.php?id=<?= $event['article-event-fk'] ?>">
-    <button class="article-event-link-btn" type="button"> Se Hendelsen </button>
-</a>
+    <a id="article-event-link" href="article-event.php?id=<?= $event['article-event-fk'] ?>">
+        <button class="article-event-link-btn" type="button"> Se Hendelsen </button>
+    </a>
+    </div>
 <!---Artikkel slutt --->
