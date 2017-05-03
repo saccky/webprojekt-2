b989c4328b68aeab4757dc1941974ca938d03a43
saccky <kennethsaccky@gmail.com>
2017-05-03 07:28:25 +0200

    Revert "test"
    This reverts commit 4a8e8ffcbd70126c443c38d6737017b308226aeb.

--- article.php
+++ article.php
@@ -1,32 +1,35 @@
-<!---Artikkel start --->
-<a href="<?= $event['article-link'] ?>" target="_blank"><?= $event['article-link-name'] ?></a>
+<?php
+    session_start();
+?>
 
-<h2><?= $event['article-title'] ?></h2>
-BY 
-<a class="av" href="<?= $event['article-author-link'] ?>" target="_blank">
-    <?= $event['article-author'] ?>
+<!---Artikkel start --->
+<a href="<?= $event['article_link'] ?>" target="_blank"><?= $event['article_link_name'] ?></a>
+
+<h2><?= $event['article_title'] ?></h2>
+av 
+<a class="av" href="<?= $event['article_author_link'] ?>" target="_blank">
+    <?= $event['article_author'] ?>
 </a>
 
 <p>
-    <img class="me" width="200" src="<?= $event['article-image-path'] ?>">
-    <?= $event['article-body'] ?>
+    <img class="me" width="200" src="<?= $event['article_image_path'] ?>">
+    <?= $event['article_body'] ?>
 </p>
 
-<!--- Sletteknapp --->
-<a class="article-delete-class" href="delete.php?id=<?= $event['id'] ?>">
-    <button class="article-delete-btn" type="button"> Slett Artikkel </button>
-</a>
+<?php
+    if(isset($_SESSION['use']))
+    { ?>
+        <!--- Sletteknapp --->
+        <a class="article-delete-class" href="delete-article.php?id=<?= $event['id'] ?>">
+            <button class="article-delete-btn" type="button"> Slett Artikkel </button>
+        </a>
+    <?php } ?>
 
-<script>
-    document.getElementById("article-event-link").style.display = "block";
-    //Gjemmer knappen hvis det ikke er noe å linke til //Virker kun på den siste artikkelen printet
-    if(<?= $event['article-event-fk'] ?>) //hvis fremmednøkkelen true (ikke er null), vis knapp
-    {
-        document.getElementById("article-event-link").style.display = "block";
-    }
-</script>
-
-<a id="article-event-link" href="article-event.php?id=<?= $event['article-event-fk'] ?>">
-    <button class="article-event-link-btn" type="button"> Se Hendelsen </button>
-</a>
-<!---Artikkel slutt --->
+<?php
+    //hvis fremmednøkkelen er true (ikke er null), legg til knapp
+    if($event['article_event_fk']) 
+    { ?>
+        <a id="article-event-link" href="article-event.php?id=<?= $event['article_event_fk'] ?>">
+            <button class="article-event-link-btn" type="button"> Se Hendelsen </button>
+        </a>
+    <?php } ?>
--- delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require 'connection-header.php';
-    $statement = $connection->prepare('DELETE FROM articles WHERE id =' .$_GET['id']);
-    require 'connection-footer.php';
-
-    header('Location: index.php');
-    exit;
