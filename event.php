a67cdce12b2be369e6a2b1489d1b62d1cc1f902c
grdall <garale16@student.westerdals.no>
2017-05-04 12:50:00 +0200

    Event stil

--- event.php
+++ event.php
@@ -4,27 +4,29 @@
 
 <!--- Event start --->
 <div class="article-shadow">
-    <img class="event_image" width="300" height="200" src="<?= $event['event_image_path'] ?>">
+    <div class="article-container">
+        <img class="event_image" width="300" height="200" src="<?= $event['event_image_path'] ?>">
 
-    <h2><?= $event['event_title'] ?></h2> 
-    <a class="av" href="<?= $event['event_where_link'] ?>" target="_blank">
-        <?= $event['event_where'] ?>
-    </a>
+        <h2><?= $event['event_title'] ?></h2> 
+        <a class="av" href="<?= $event['event_where_link'] ?>" target="_blank">
+            <?= $event['event_where'] ?>
+        </a>
 
-    <p>
-        <?= $event['event_body'] ?>
-    </p>
-</div>
+        <p>
+            <?= $event['event_body'] ?>
+        </p>
 
 <?php
-    if(isset($_SESSION['use']))
-    { ?>
-        <!--- Sletteknapp --->
-        <a class="event-delete-class" href="delete-event.php?id=<?= $event['id'] ?>">
-            <button class="event-btn" type="button"> Slett Event </button>
-        </a>
-        <!--- Editknapp --->
-        <a class="event-edit-class" href="edit-event.php?id=<?= $event['id'] ?>">
-            <button class="event-btn" type="button"> Endre Event </button>
-        </a>
-    <?php } ?>
+        if(isset($_SESSION['use']))
+        { ?>
+            <!--- Sletteknapp --->
+            <a class="event-delete-class" href="delete-event.php?id=<?= $event['id'] ?>">
+                <button class="event-btn" type="button"> Slett Event </button>
+            </a>
+            <!--- Editknapp --->
+            <a class="event-edit-class" href="edit-event.php?id=<?= $event['id'] ?>">
+                <button class="event-btn" type="button"> Endre Event </button>
+            </a>
+<?php } ?>
+    </div>
+</div>
--- new-event.php
+++ new-event.php
@@ -1,5 +1,5 @@
 <?php 
-    require 'header.php' 
+    require 'header.php';
 ?>
 
 <br>
