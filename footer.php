c661897ad60e80fa0a34cfa7a6150c7b0e014683
saccky <kennethsaccky@gmail.com>
2017-05-04 11:14:12 +0200

    Gruppe istället för grupp

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
                     <img class="logoer" id="facelogo" src="./logoer/face.png">
                     <img class="logoer" id="twitterlogo" src="./logoer/twitt.png">
                     <img class="logoer" id="instalogo" src="./logoer/insta.png">
-                    <div id="footertext">&copy;2017 Laget av Grupp 32</div>
+                    <div id="footertext">&copy;2017 Laget av Gruppe 32</div>
                 </footer>
 
         <!---/div> <!--- #wrapper lukker --->
