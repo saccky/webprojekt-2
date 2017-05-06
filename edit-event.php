7bdad88b2e6948d66c913dafed8739740291b0c7
Alexander <garale16@student.westerdals.no>
2017-05-06 18:24:57 +0200

    Sjekker om felt er utfylt event

--- edit-event.php
+++ edit-event.php
@@ -3,39 +3,37 @@
     $statement = $connection->prepare('SELECT * FROM events WHERE id =' .$_GET['id']);
     require 'connection-footer.php';
     require 'header.php';
-
 ?>
-    <div class="box">
-<?php
-
+<div class="box">
+        
+    <?php
     foreach ($events as $event) //Unødvendig, annen måte i hente verdier som blir endret til "$event" her?
-    {    
-?>
-        <br>
+    { 
+        echo 'Fyll ut alle felt market med en stjerne(*)<br>'; ?>
         <main class="form-edit">
             <div class="">
                 <form class="" action="update-event.php" method="post">
                     <div class="">
                         <input class="" type="text" id="edit-title" name="edit-title" value="<?= $event['event_title'] ?>">
-                        <label class="">Tittel</label>
+                        <label class="">Tittel*</label>
                     </div>
                     <div class="">
                         <input class="" type="text" id="edit-where" name="edit-where" value="<?= $event['event_where'] ?>">
-                        <label class="">Hvor</label>
+                        <label class="">Hvor*</label>
                     </div>
                     <div class="">
                         <input class="" type="text" id="edit-where-link" name="edit-where-link" value="<?= $event['event_where_link'] ?>">
-                        <label class="">Link til hvor (null)</label>
+                        <label class="">Link til hvor</label>
                     </div>
                     <div class="">
                         <input class="" type="datetime" id="edit-datetime" name="edit-datetime" value="<?= $event['event_datetime'] ?>">
-                        <label class="">Naar</label>
+                        <label class="">Naar*</label>
                     </div>
                     <div class="">
                         <textarea class="" type="text" rows= "10" id="edit-body" name="edit-body">
                             <?= $event['event_body']?>
                         </textarea>
-                        <label class="" for="body">Body</label>
+                        <label class="" for="body">Body*</label>
                     </div>
                     <div class="">
                         <input class="" type="text" id="edit-image-path" name="edit-image-path" value="<?= $event['event_image_path'] ?>">
@@ -49,18 +47,18 @@
                             <option value="kultur">Kultur</option>
                             <option value="skole">Skole</option>
                         </select>
-                        <label class="">Tag</label>
+                        <label class="">Tag*</label>
                     </div>
-                    <button class="">
-                        Endre
+                    <button class="" onclick="checkInput()">
+                        Lagre
                     </button>
                     <input type="hidden" name="edit-event-id" value="<?= $_GET['id']?>"> <!--- Henter ID og sender den videre --->
                 </form>
             </div>
         </main>
-    </div>
-<?php
-    }
+    <?php } ?>
+</div>
 
+<?php
     require 'footer.php'; 
 ?>
--- new-event.php
+++ new-event.php
@@ -3,29 +3,29 @@
 ?>
 
 <div class="box">
-    <br>
+    <?= 'Fyll ut alle felt market med en stjerne(*)<br>' ?>
     <main class="form-new">
         <div class="">
             <form class="" action="store-event.php" method="post">
                 <div class="">
                     <input class="" type="text" id="new-title" name="new-title">
-                    <label class="">Tittel</label>
+                    <label class="">Tittel*</label>
                 </div>
                 <div class="">
                     <input class="" type="text" id="new-where" name="new-where">
-                    <label class="">Hvor</label>
+                    <label class="">Hvor*</label>
                 </div>
                 <div class="">
                     <input class="" type="text" id="new-where-link" name="new-where-link">
-                    <label class="">Link til hvor (null)</label>
+                    <label class="">Link til hvor</label>
                 </div>
                 <div class="">
                     <input class="" type="datetime-local" rows= "10" id="new-datetime" name="new-datetime"> <!--- datetime-local virker ikke i firefox, eldre IE versjoner --->
-                    <label class="">Naar</label>
+                    <label class="">Naar*</label>
                 </div>
                 <div class="">
                     <textarea class="" type="text" rows= "10" id="new-body" name="new-body"></textarea>
-                    <label class="" for="body">Body</label>
+                    <label class="" for="body">Body*</label>
                 </div>
                 <div class="">
                     <input class="" type="text" id="new-image-path" name="new-image-path">
@@ -38,7 +38,7 @@
                         <option value="kultur">Kultur</option>
                         <option value="skole">Skole</option>
                     </select>
-                    <label class="">Tag</label>
+                    <label class="">Tag*</label>
                 </div>
                 <button class="">
                     Legg til
