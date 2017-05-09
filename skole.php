9d7c1448a69ae21c34303c5bd76d6af387a7d2fc
saccky <kennethsaccky@gmail.com>
2017-05-09 17:42:27 +0200

    test

--- skole.php
+++ skole.php
@@ -12,10 +12,11 @@
    <p> 
        The opening of a streetwear giant such as Palace would naturally draw a line, but no one would expect the turn out to be so large on May 5, 2017. The shop, located at 49 Howard St. neatly between Stadium Goods and around the corner from NikeLab, was announced earlier with a teaser video and later confirmed with a solid address and store hours.Regardless of the torrential rain, hopeful shoppers stood in line and donned their latest pickups ranging from Supreme and Anti Social Social Club to BAPE and even Palace pieces from previous seasons and collections. 
    </p>
-        
+      
     </div>
-    
+
     <div class="item-2 item">
+        
         <h1>Länkar</h1>
         <a href="http://www.westerdals.no" target="_blank">Westerdals hjemmeside</a><br>
         <a href="https://trello.com/b/8BqHB93W/kreativt-webprosjekt2" target="_blank">Trello</a>
