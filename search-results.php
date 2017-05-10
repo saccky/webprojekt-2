5f708f4bf6b0577b732b2812a864be2ea3b5c36a
Alexander <garale16@student.westerdals.no>
2017-05-10 13:24:03 +0200

    Søke på navn, email, info av forfattere

--- search-results.php
+++ search-results.php
@@ -6,7 +6,14 @@
     $articlecounter = 0; //Teller artikkler printet
     $getsearch = $_GET['search'];
 
-    $statement = $connection->prepare('SELECT * FROM events 
+    $statement = $connection->prepare('SELECT * FROM articles 
+    WHERE id = "'.$getsearch.'" 
+    OR article_title LIKE "%'.$getsearch.'%"  
+    OR article_author LIKE "%'.$getsearch.'%" 
+    ORDER BY id DESC');
+    //order by body, date, link_name?
+
+    $statement2 = $connection->prepare('SELECT * FROM events 
     WHERE id = "'.$getsearch.'" 
     OR event_title LIKE "%'.$getsearch.'%" 
     OR event_where LIKE "%'.$getsearch.'%" 
@@ -14,24 +21,31 @@
     ORDER BY ID DESC');
     //order by date, body?
 
-    $statement2 = $connection->prepare('SELECT * FROM articles 
+    $statement3 = $connection->prepare('SELECT * FROM authors
     WHERE id = "'.$getsearch.'" 
-    OR article_title LIKE "%'.$getsearch.'%"  
-    OR article_author LIKE "%'.$getsearch.'%" 
-    ORDER BY id DESC');
-    //order by body, date, link_name?
+    OR author_name LIKE "%'.$getsearch.'%"  
+    OR author_email LIKE "%'.$getsearch.'%" 
+    OR author_info LIKE "%'.$getsearch.'%"');
 
     require 'connection-footer.php';
     require 'connection-footer-2.php';
+    $statement3->execute();
+    $events3 = [];
+
+    while($row3 = $statement3->fetch(PDO::FETCH_ASSOC)) 
+    {
+        $events3[] = $row3;
+    } 
 
     require 'header.php';
-    ?>
+?>
 
+<div class="wrap">
     <div class="box">
         <h1> Viser tag/id resultater for: <?= $getsearch ?> </h1>
 
         <?php
-        foreach ($events2 as $event) 
+        foreach ($events as $event) 
         {   
             require 'article.php';
             echo "<br>";
@@ -46,7 +60,7 @@
         <hr>
 
         <?php
-        foreach ($events as $event) 
+        foreach ($events2 as $event) 
         {
             require 'event.php';
             echo "<br>";
@@ -56,7 +70,23 @@
         {
             echo "Beklager! Vi fant ingen hendelser med $getsearch!";
         }?>
+
+        <br>
+        <hr>
+
+        <?php
+        foreach ($events3 as $event) 
+        {
+            require 'author.php';
+            echo "<br>";
+            $authorcounter++;
+        } 
+        if($authorcounter == 0)
+        {
+            echo "Beklager! Vi fant ingen forfattere med $getsearch!";
+        }?>
     </div>
+</div>
 
 <?php
     require 'footer.php';
