35f8b835a77b18ab9fcbed82f624eb0c63b95a96
Alexander <garale16@student.westerdals.no>
2017-05-28 14:19:13 +0200

    småfiks

--- employee.php
+++ employee.php
@@ -12,14 +12,14 @@ header('Content-Type: text/html; charset=ISO-8859-1');
         <br>
         <a href="mailto:<?= $event['employee_email'] ?>"><?= $event['employee_email'] ?></a>  
         <br>
-        Information: <?= $event['employee_info'] ?>
+        Informasjon: <?= $event['employee_info'] ?>
     </div>        
     
     <br><br><br>
     <div class="employee-buttons">
         <?php
         //hvis fremmednøkkelen er true (ikke er null), legg til knapp
-        if($event['']) //Linke til artikler skrevet av forfatter/ansatt"
+        if($event['']) //Linke til artikler skrevet av forfatter/ansatt
         { ?>
             <a id="employee-article-link" href="employee-article.php?id=<?= $event[''] ?>">
                 <button class="employee-btn" type="button"> Se Artikler (placeholder) </button>
